520db54232ed1cbb2246d79c07229b98c9db4f01
hmspisharody <hari.iitman@gmail.com>
2018-10-09 00:27:40 -0700

    fixed issues with python 3 (progressive filter for pytube.YouTube

--- playlist-downloader.py
+++ playlist-downloader.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os, sys, time
 import argparse
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 
 import pytube  # pip install pytube
 
@@ -10,7 +10,9 @@ CS294_playlist_url = "https://www.youtube.com/playlist?list=PLkFD6_40KJIwTmSbCv9
 
 
 def get_playlist_links(playlist_url):
-    page_elements = urlopen(playlist_url).readlines()
+    request = Request(playlist_url)
+    page_elements = urlopen(request).readlines()
+    page_elements = [x.decode('utf-8') for x in page_elements]
     video_elements = [el for el in page_elements if 'pl-video-title-link' in el]  # Filter out unnecessary lines
     video_urls = [v.split('href="',1)[1].split('" ',1)[0] for v in video_elements]  # Grab the video urls from the elements
     return ['http://www.youtube.com' + v for v in video_urls]
@@ -25,7 +27,6 @@ def print_dot(bytes_received, file_size, start):
         sys.stdout.flush()
         start_time = time.time()
 
-
 parser = argparse.ArgumentParser(usage='%(prog)s [-h] [-p PLAYLISTURL] [-d DESTINATION]')
 parser.add_argument('-p', '--playlisturl', help='url of the playlist to be downloaded', default=CS294_playlist_url, metavar='')
 parser.add_argument('-d', '--destination', help='path of directory to save videos to', default=os.path.curdir, metavar='')
@@ -39,13 +40,13 @@ else:
     sys.exit(1)
 
 video_urls = get_playlist_links(args.playlisturl)
-confirmation = raw_input('You are about to download {} videos to {}\nWould you like to continue? [Y/n] '.format(
+confirmation = input('You are about to download {} videos to {}\nWould you like to continue? [Y/n] '.format(
     len(video_urls), os.path.abspath(args.destination)))
 
 if confirmation.lower() in ['y', '']:
     for u in video_urls:
         yt = pytube.YouTube(u)
-        vid = yt.streams.filter(file_extension='mp4').order_by('res').last() # grab the highest resolution mp4 file
+        vid = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first() # grab the highest resolution mp4 file
 
         if vid.default_filename in directory_contents:
             print('Skipping {}'.format(vid.default_filename))
